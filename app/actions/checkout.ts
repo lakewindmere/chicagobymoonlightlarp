4f6fc0d3243a26a6516100e87629bdbfbe6d8f4d
lakewindmere <72631099+lakewindmere@users.noreply.github.com>
2026-05-01 12:23:31 -0500

    fixed url parsing error

--- app/actions/checkout.ts
+++ app/actions/checkout.ts
@@ -5,6 +5,7 @@ import { redirect } from 'next/navigation';
 const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 
 export async function handleCheckout(priceId: string) {
+  console.warn(`${baseUrl}/success?session_id={CHECKOUT_SESSION_ID}`);
   const session = await stripe.checkout.sessions.create({
     line_items: [
       {
@@ -14,8 +15,8 @@ export async function handleCheckout(priceId: string) {
     ],
     mode: 'payment',
     // Change these to your actual domain later
-    success_url: `{baseUrl}/success?session_id={CHECKOUT_SESSION_ID}`,
-    cancel_url: `{baseUrl}/store`,
+    success_url: `${baseUrl}/success?session_id={CHECKOUT_SESSION_ID}`,
+    cancel_url: `${baseUrl}/store`,
   });
 
   if (session.url) {
