3d95339884142e37b87a1472b993b6be4e752962
lakewindmere <72631099+lakewindmere@users.noreply.github.com>
2026-05-01 12:17:59 -0500

    swapped local url for vercel

--- app/actions/checkout.ts
+++ app/actions/checkout.ts
@@ -2,6 +2,8 @@
 import { stripe } from '@/lib/stripe';
 import { redirect } from 'next/navigation';
 
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+
 export async function handleCheckout(priceId: string) {
   const session = await stripe.checkout.sessions.create({
     line_items: [
@@ -12,8 +14,8 @@ export async function handleCheckout(priceId: string) {
     ],
     mode: 'payment',
     // Change these to your actual domain later
-    success_url: `http://localhost:3000/success?session_id={CHECKOUT_SESSION_ID}`,
-    cancel_url: `http://localhost:3000/store`,
+    success_url: `{baseUrl}/success?session_id={CHECKOUT_SESSION_ID}`,
+    cancel_url: `{baseUrl}/store`,
   });
 
   if (session.url) {
